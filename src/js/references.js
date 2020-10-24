8f4189ff14fb50b495768b01b3191fed6ac2b4ff
Paul Chávez <paulchavezromero20@gmail.com>
2020-10-24 16:11:36 -0500

    references added

---
+++ src/js/references.js
@@ -0,0 +1,12 @@
+// References
+const container = document.querySelector( '.container' ),
+      result    = document.querySelector( '.resultado' ),
+      form      = document.querySelector( '#formulario' ),
+      city      = document.querySelector( '#ciudad' ),
+      country   = document.querySelector( '#pais' );
+
+
+export {
+    container,
+    form
+}
