8b6845be2df34855bce752562f073b0b56453a2c
Paul Chávez <paulchavezromero20@gmail.com>
2020-10-24 17:43:31 -0500

    consultAPI added, consult the API

--- src/js/functions.js
+++ src/js/functions.js
@@ -2,7 +2,7 @@ import { cityInput, countryInput, ui } from './references.js';
 
 // Functions
 // Valida el formulario
-const validateForm = ( event ) => {
+const validateForm = event => {
     event.preventDefault();
 
     const city      = cityInput.value,
@@ -13,6 +13,24 @@ const validateForm = ( event ) => {
         return;
     }
 
+    consultAPI( city, country );
+}
+
+// Consulta la API
+const consultAPI = ( city, country ) => {
+    const appID = '9483abbc0441d5dc4b4944a5e211fa35';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${ city },${ country }&appid=${ appID }`;
+    
+    fetch( url )
+        .then( response => response.json() )
+        .then( data => {
+            if( data.cod === '404' ) {
+                ui.showError( 'Ciudad no encontrada' );
+                return;
+            }
+
+            ui.showClimate( data );
+        })
 }
 
 export {
